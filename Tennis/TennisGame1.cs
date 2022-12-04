31d573568d03dfb5b3658e28e0eec49ce9340574
Gwendal Lasson <gwendal.lasson@gmail.com>
2022-12-04 18:41:35 +0100

    introduce Umpire class

--- Tennis/TennisGame1.cs
+++ Tennis/TennisGame1.cs
@@ -4,11 +4,13 @@ namespace Tennis
     {
         private readonly Player Player1;
         private readonly Player Player2;
+        private readonly Umpire Umpire;
 
         public TennisGame1(string player1Name, string player2Name)
         {
-            this.Player1 = new Player(player1Name, 0);
-            this.Player2 = new Player(player2Name, 0);
+            Player1 = new Player(player1Name, 0);
+            Player2 = new Player(player2Name, 0);
+            Umpire = new Umpire();
         }
 
         public void WonPoint(string playerName)
@@ -18,56 +20,12 @@ namespace Tennis
 
         public string GetScore()
         {
-            if (Player1.GetScore() == Player2.GetScore())
-            {
-                return EstablishDrawScore();
-            }
-            if (Player1.GetScore() >= 4 || Player2.GetScore() >= 4)
-            {
-                return EstablishEndGameScore();
-            }
-            return EstablishNormalScore();
+            return Umpire.GetScore(Player1, Player2);
         }
 
         private Player GetPlayerByName(string name)
         {
             return name == "player1" ? Player1 : Player2;
-        }
-
-        private string EstablishNormalScore()
-        {
-            return $"{GetScoreName(Player1.GetScore())}-{GetScoreName(Player2.GetScore())}";
-        }
-
-        private string EstablishEndGameScore()
-        {
-            var scoreDelta = Player1.GetScore() - Player2.GetScore();
-            if (scoreDelta == 1) return "Advantage player1";
-            if (scoreDelta == -1) return "Advantage player2";
-            if (scoreDelta >= 2) return "Win for player1";
-            return "Win for player2";
-        }
-
-        private string EstablishDrawScore()
-        {
-            return Player1.GetScore() switch
-            {
-                0 => "Love-All",
-                1 => "Fifteen-All",
-                2 => "Thirty-All",
-                _ => "Deuce",
-            };
-        }
-
-        private static string GetScoreName(int score)
-        {
-            return score switch
-            {
-                0 => "Love",
-                1 => "Fifteen",
-                2 => "Thirty",
-                _ => "Forty",
-            };
         }
     }
 }
---
+++ Tennis/Umpire.cs
@@ -0,0 +1,57 @@
+﻿namespace Tennis
+{
+    sealed class Umpire
+    {
+        public string GetScore(Player player1, Player player2)
+        {
+            int player1Score = player1.GetScore();
+            int player2Score = player2.GetScore();
+            if (player1Score == player2Score)
+            {
+                return EstablishDrawScore(player1Score);
+            }
+            if (player1Score >= 4 || player2Score >= 4)
+            {
+                return EstablishEndGameScore(player1Score, player2Score);
+            }
+            return EstablishNormalScore(player1Score, player2Score);
+        }
+
+            private string EstablishNormalScore(int player1Score, int player2Score)
+            {
+                return $"{GetScoreName(player1Score)}-{GetScoreName(player2Score)}";
+            }
+
+            private string EstablishEndGameScore(int player1Score, int player2Score)
+            {
+                var scoreDelta = player1Score - player2Score;
+                if (scoreDelta == 1) return "Advantage player1";
+                if (scoreDelta == -1) return "Advantage player2";
+                if (scoreDelta >= 2) return "Win for player1";
+                return "Win for player2";
+            }
+
+            private string EstablishDrawScore(int score)
+            {
+                return score switch
+                {
+                    0 => "Love-All",
+                    1 => "Fifteen-All",
+                    2 => "Thirty-All",
+                    _ => "Deuce",
+                };
+            }
+
+            private static string GetScoreName(int score)
+            {
+                return score switch
+                {
+                    0 => "Love",
+                    1 => "Fifteen",
+                    2 => "Thirty",
+                    _ => "Forty",
+                };
+            }
+    }
+}
+
