b8f009d84bb695a27fdbc52d35b7d16aebdedcad
Gwendal Lasson <gwendal.lasson@gmail.com>
2022-12-04 18:11:31 +0100

    refacto: extract edge cases from GetScore method

--- Tennis/TennisGame1.cs
+++ Tennis/TennisGame1.cs
@@ -2,8 +2,8 @@ namespace Tennis
 {
     public class TennisGame1 : ITennisGame
     {
-        private int m_score1 = 0;
-        private int m_score2 = 0;
+        private int player1Score = 0;
+        private int player2Score = 0;
         private string player1Name;
         private string player2Name;
 
@@ -16,65 +16,83 @@ namespace Tennis
         public void WonPoint(string playerName)
         {
             if (playerName == "player1")
-                m_score1 += 1;
+                player1Score += 1;
             else
-                m_score2 += 1;
+                player2Score += 1;
         }
 
         public string GetScore()
         {
-            string score = "";
-            var tempScore = 0;
-            if (m_score1 == m_score2)
+            if (player1Score == player2Score)
             {
-                switch (m_score1)
+                return EstablishDrawScore();
+            }
+            if (player1Score >= 4 || player2Score >= 4)
+            {
+                return EstablishEndGameScore();
+            }
+            return EstablishNormalScore();
+        }
+
+        private string EstablishNormalScore()
+        {
+            var score = "";
+            for (var i = 1; i < 3; i++)
+            {
+                int tempScore;
+                if (i == 1) tempScore = player1Score;
+                else { score += "-"; tempScore = player2Score; }
+                switch (tempScore)
                 {
                     case 0:
-                        score = "Love-All";
+                        score += "Love";
                         break;
                     case 1:
-                        score = "Fifteen-All";
+                        score += "Fifteen";
                         break;
                     case 2:
-                        score = "Thirty-All";
+                        score += "Thirty";
                         break;
-                    default:
-                        score = "Deuce";
+                    case 3:
+                        score += "Forty";
                         break;
+                }
+            }
 
-                }
-            }
-            else if (m_score1 >= 4 || m_score2 >= 4)
+            return score;
+        }
+
+        private string EstablishEndGameScore()
+        {
+            string score;
+            var minusResult = player1Score - player2Score;
+            if (minusResult == 1) score = "Advantage player1";
+            else if (minusResult == -1) score = "Advantage player2";
+            else if (minusResult >= 2) score = "Win for player1";
+            else score = "Win for player2";
+            return score;
+        }
+
+        private string EstablishDrawScore()
+        {
+            string score;
+            switch (player1Score)
             {
-                var minusResult = m_score1 - m_score2;
-                if (minusResult == 1) score = "Advantage player1";
-                else if (minusResult == -1) score = "Advantage player2";
-                else if (minusResult >= 2) score = "Win for player1";
-                else score = "Win for player2";
+                case 0:
+                    score = "Love-All";
+                    break;
+                case 1:
+                    score = "Fifteen-All";
+                    break;
+                case 2:
+                    score = "Thirty-All";
+                    break;
+                default:
+                    score = "Deuce";
+                    break;
+
             }
-            else
-            {
-                for (var i = 1; i < 3; i++)
-                {
-                    if (i == 1) tempScore = m_score1;
-                    else { score += "-"; tempScore = m_score2; }
-                    switch (tempScore)
-                    {
-                        case 0:
-                            score += "Love";
-                            break;
-                        case 1:
-                            score += "Fifteen";
-                            break;
-                        case 2:
-                            score += "Thirty";
-                            break;
-                        case 3:
-                            score += "Forty";
-                            break;
-                    }
-                }
-            }
+
             return score;
         }
     }
